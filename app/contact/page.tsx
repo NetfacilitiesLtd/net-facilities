6b971e3b59a6527460f4ff5dcd34244146cf8212
netfacilitiesLtd <netfacilities26@gmail.com>
2026-07-13 17:39:56 -0500

    Move Google Map to contact page only

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,10 @@
 import ContactSection from "../../components/contact/ContactSection";
-
+import GoogleMap from "../../components/GoogleMap";
 export default function ContactPage() {
   return (
     <main className="bg-white">
-      <ContactSection />
-    </main>
+  <ContactSection />
+  <GoogleMap />
+</main>
   );
 }
--- components/contact/ContactSection.tsx
+++ components/contact/ContactSection.tsx
@@ -202,17 +202,7 @@ const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
 
   </div>
 
-  <div className="mt-20 overflow-hidden rounded-3xl shadow-xl">
-    <iframe
-      src="https://www.google.com/maps?q=LB8+Calcite+Close,+Tesano,+Accra&output=embed"
-      width="100%"
-      height="450"
-      style={{ border: 0 }}
-      loading="lazy"
-      referrerPolicy="no-referrer-when-downgrade"
-      title="NET Facilities Office Location"
-    />
-  </div>
+  
 
 </div>
     </section>
