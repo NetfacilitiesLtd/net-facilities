cbc40f908b7e268489a87b6c5f65d801a9d30bd1
netfacilitiesLtd <netfacilities26@gmail.com>
2026-07-13 11:02:34 -0500

    Remove duplicate navbar and footer from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,21 @@ import BackToTop from "@/components/BackToTop";
 import Gallery from "@/components/Gallery";
 import WhatsAppButton from "@/components/WhatsAppButton";
 import Process from "@/components/Process";
-import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
   <LoadingScreen />
-  <Navbar />
   <Hero />
   <Services />
   <WhyChooseUs />
   <Gallery />
   <Process />
   <Contact />
-  <Footer />
   <BackToTop />
 </main>
   );
