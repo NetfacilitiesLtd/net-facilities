f43c88469db4d7869b370de978386e5e391a483d
netfacilitiesLtd <netfacilities26@gmail.com>
2026-07-16 14:55:37 -0500

    Enhance homepage, regional reach and SEO configuration

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
     },
 
-    sitemap: "https://www.netfacilities.com/sitemap.xml",
+    sitemap: "https://www.netfacilitiesmanagement.com/sitemap.xml",
   };
 }
